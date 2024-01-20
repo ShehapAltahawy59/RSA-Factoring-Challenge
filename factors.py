9fd0db6a828fbcae9565d94e91b28c4e869ae785
shehap altahawy <shehapkhalil62@gmail.com>
2024-01-20 06:26:49 -0800

    edit

--- factors.py
+++ factors.py
@@ -1,10 +1,25 @@
 import sys
+import math
+import random
 
-def factorize(n):
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return i, n // i
-    return 1, n
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+def pollards_rho(n):
+    if n % 2 == 0:
+        return 2
+    x = random.randint(1, n-1)
+    y = x
+    c = random.randint(1, n-1)
+    g = 1
+    while g==1:             
+        x = ((x * x) % n + c) % n
+        y = ((y * y) % n + c) % n
+        y = ((y * y) % n + c) % n
+        g = gcd(abs(x-y), n)
+    return g
 
 def main():
     filename = sys.argv[1]
@@ -12,7 +27,8 @@ def main():
         numbers = [int(line.strip()) for line in file]
 
     for number in numbers:
-        p, q = factorize(number)
+        p = pollards_rho(number)
+        q = number // p
         print(f'{number}={p}*{q}')
 
 if __name__ == "__main__":
